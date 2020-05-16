f7db8431d81286b6b0349cf1b0824769691ef68f
Remi Shergold <remi.shergold@gmail.com>
2020-05-16 19:49:09 +0100

    Remove .

--- dist/index.php
+++ dist/index.php
@@ -18,7 +18,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Token Burger.</title>
+  <title>Token Burger</title>
   <style>
   body {
     background-color: #3b4652;
